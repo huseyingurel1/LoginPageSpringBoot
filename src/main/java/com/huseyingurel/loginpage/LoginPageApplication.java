9b312bda4f56e8c82d8b24db1841722438cf64ff
huseyingurel1 <huseyingurel27@gmail.com>
2022-05-26 18:08:15 +0300

    First commit

---
+++ src/main/java/com/huseyingurel/loginpage/LoginPageApplication.java
@@ -0,0 +1,12 @@
+package com.huseyingurel.loginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPageApplication {
+
+    public static void main(String[] args) {SpringApplication.run(LoginPageApplication.class, args);
+    }
+
+}
